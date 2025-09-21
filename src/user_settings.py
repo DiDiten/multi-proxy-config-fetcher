86ee23d980b7e33378dc8e5afbd8cf4bce25eeff
DiDiten <217420093+DiDiten@users.noreply.github.com>
2025-09-21 16:52:12 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -1,20 +1,57 @@
-# Please modify the settings below according to your needs.
-
 # List of source URLs to fetch proxy configurations from.
-# Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
-    "https://raw.githubusercontent.com/4n0nymou3/wg-config-fetcher/refs/heads/main/configs/wireguard_configs.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_1.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_2.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_3.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_4.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_2.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_3.txt",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_4.txt",
     "https://raw.githubusercontent.com/Rayan-Config/C-Sub/refs/heads/main/configs/proxy.txt",
+    "https://raw.githubusercontent.com/DiDiten/HiN-VPN/main/subscription/base64/mix",
+    "https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_base64_Sub.txt",
+    "https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/subscriptions/v2ray/super-sub.txt",
+    "https://robin.victoriacross.ir",
+    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity",
+    "https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/subscriptions/v2ray/all_sub.txt",
+    "https://dev1.irdevs.sbs",
+    "https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/mix/sub.html",
+    "https://raw.githubusercontent.com/lagzian/SS-Collector/refs/heads/main/SS/TrinityBase",
+    "https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/refs/heads/main/sub/vless.txt",
+    "https://raw.githubusercontent.com/nscl5/5/refs/heads/main/configs/all.txt",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/refs/heads/main/V2RAY_RAW.txt",
+    "https://raw.githubusercontent.com/Freedom-Guard-Builder/Freedom-Finder/refs/heads/main/out/mixed_configs.txt",
+    "https://raw.githubusercontent.com/F0rc3Run/F0rc3Run/refs/heads/main/splitted-by-country/XX.txt",
 
+
+
+"https://t.me/s/v2ray_Extractor",
+"https://t.me/s/BINNER_IRAN",
+"https://t.me/s/DeamNet",
+"https://t.me/s/AzadNet",
+"https://t.me/s/freeconfigsplus",
+"https://t.me/s/configshubplus",
+"https://t.me/s/Config_Vortex55",
+"https://t.me/s/configshubplus",
+"https://t.me/s/APPXA",
+"https://t.me/s/arshia_mod_fun",
+"https://t.me/s/lightning6",
+"https://t.me/s/spotify_porteghali",
+"https://t.me/s/kvetch_matin",
+"https://t.me/s/hacknashid",
+"https://t.me/s/lnrbymaa",
+"https://t.me/s/lyricpixelart",
+"https://t.me/s/madshopx",
+"https://t.me/s/movie10_oficial",
+"https://t.me/s/mbtiuniverse",
+"https://t.me/s/mitivpn",
+"https://t.me/s/mrsoulb",
+"https://t.me/s/prooofsor",
+"https://t.me/s/soskeynet",
+"https://t.me/s/speeds_vpn1",
+"https://t.me/s/surfboardv2ray",
+"https://t.me/s/v2ray1_ng",
+"https://t.me/s/v2ray_vpn_ir",
+"https://t.me/s/vless_config",
+"https://t.me/s/vlessconfight",
+"https://t.me/s/vpn4everyone",
+"https://t.me/s/vpnbaz",
+"https://t.me/s/vpnserverrr",
+"https://t.me/s/vpn_solve",
+"https://t.me/s/xixv2ray",
 "https://t.me/s/ahwazigamingshop",
 "https://t.me/s/ar14n24b",
 "https://t.me/s/betv2ray",
@@ -38,7 +75,6 @@ SOURCE_URLS = [
 "https://t.me/s/vless_config",
 "https://t.me/s/vpn_solve",
 "https://t.me/s/vpnbaz"
-    # Add more URLs here if you want to include additional sources.
 ]
 
 # Set to True to fetch the maximum possible number of configurations.
@@ -47,7 +83,7 @@ USE_MAXIMUM_POWER = False
 
 # Desired number of configurations to fetch.
 # This is used only if USE_MAXIMUM_POWER is False.
-SPECIFIC_CONFIG_COUNT = 600
+SPECIFIC_CONFIG_COUNT = 800
 
 # Dictionary of protocols to enable or disable.
 # Set each protocol to True to enable, False to disable.
